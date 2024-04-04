8f8fdefab6b72f8c965ee47611c5c1e6e9fa45ba
cedricscbjj <63507246+cedricscbjj@users.noreply.github.com>
2024-04-04 14:45:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Datalink = require('./datamodel')
 
 const app = express()
-const port = 3405
+const port = 3000
 const cors = require('cors');
 
 const mongoose = require('mongoose')

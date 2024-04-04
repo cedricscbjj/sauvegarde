d7e3e21d3d7dabe48026a72f711c2fdf9cb536da
cedricscbjj <63507246+cedricscbjj@users.noreply.github.com>
2024-04-04 14:44:51 +0200

    Update postdata.js

--- postdata.js
+++ postdata.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import './index.css';
 import axios from 'axios';
-import './App.css';
 
-const AddDataForm = () => {
+
+
+const Postdata = () => {
+
   const [lien, setLien] = useState('');
   const [description, setDescription] = useState('');
   const [theme1, setTheme1] = useState('');
@@ -11,40 +14,50 @@ const AddDataForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3405/datalinks', { lien, description, theme1, theme2 });
+      const response = await axios.post('http://localhost:3000/datalinks', { lien, description, theme1, theme2 });
       console.log(response.data);
-      // do something with response if needed
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
+    <div>
+
+<div>bienvenue sur la page de sauvegarde de données</div>
+
+<p> Vos contenus YouTube apparaissent ici. Il peut s'agir des vidéos que vous avez mises en ligne, des commentaires que vous avez ajoutés à des vidéos ou encore des chaînes auxquelles vous êtes abonné.
+
+    
+</p>
 
 
+  
 
 
-    <form onSubmit={handleSubmit}>
-      <h1> poster un lien</h1>
-      <label>
-        Lien:
-        <input type="text" value={lien} onChange={(e) => setLien(e.target.value)} />
-      </label>
-      <label>
-        Description:
-        <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
-      </label>
-      <label>
-        Theme 1:
-        <input type="text" value={theme1} onChange={(e) => setTheme1(e.target.value)} />
-      </label>
-      <label>
-        Theme 2:
-        <input type="text" value={theme2} onChange={(e) => setTheme2(e.target.value)} />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
+<form onSubmit={handleSubmit} className="form-container">
+  <h1>Poster un lien</h1>
+  <div className="form-group">
+    <label htmlFor="lien">Lien:</label>
+    <input type="text" id="lien" value={lien} onChange={(e) => setLien(e.target.value)} />
+  </div>
+  <div className="form-group">
+    <label htmlFor="description">Description:</label>
+    <textarea id="description" value={description} onChange={(e) => setDescription(e.target.value)} />
+  </div>
+  <div className="form-group">
+    <label htmlFor="theme1">Theme 1:</label>
+    <input type="text" id="theme1" value={theme1} onChange={(e) => setTheme1(e.target.value)} />
+  </div>
+  <div className="form-group">
+    <label htmlFor="theme2">Theme 2:</label>
+    <input type="text" id="theme2" value={theme2} onChange={(e) => setTheme2(e.target.value)} />
+  </div>
+  <button type="submit">Submit</button>
+</form>
+</div>
 
-export default AddDataForm;
+  )
+}
+export default Postdata
+

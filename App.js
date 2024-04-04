d93b3e2a45bc62d7b2c4d0a50db47426ab16a16f
cedricscbjj <63507246+cedricscbjj@users.noreply.github.com>
2024-04-04 14:50:42 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,44 @@
-import './App.css';
-
-import React from 'react';
-import GetData from './getdata';
-import GetAlldata from './getalldata'
-import Postdata from './postdata'
-import Getspecificdata from './getspecificdata';
-function App() {
+import { Link } from "react-router-dom";
+import Nav from "./Nav";
+import './App.css'; //fichier de point d 'entree
+const App = () => {
   return (
-    <div>
-      <GetData />
-      <GetAlldata />
-      <Postdata />
-      <Getspecificdata />
-     
+    <div className="container">
+      <h2>Logiciel de sauvegarde de données</h2>
+
+      <br /><br />
+<div class="row">
+
+<div class="col">     <Link className="nav-link " to="/">
+         Envoyer des données
+        </Link>   </div>
+
+        <br /><br />
+<div class="col">   <Link className="nav-link " to="getspecificdata">
+          Recevoir des données ciblées
+        </Link>    </div>
+
+        <br /><br />
+<div class="col">  <Link className="nav-link" to="getalldata">
+          Recevoir toutes les données
+        </Link>     </div>
+
+</div>
+<br /><br />
+
+
+
+
+      <div>
+
+
+        <h3>
+          <Nav />
+        </h3>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
-
 
 
